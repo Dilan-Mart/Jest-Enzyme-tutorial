ec213640a206c96c7f92c6db7b18db33a510e3e4
Dilan-Mart <mdilan4518@gmail.com>
2021-11-22 10:03:08 +0530

    basic testcase

---
+++ src/ReactTest.test.js
@@ -0,0 +1,42 @@
+import sum from './ReactTest.js';
+
+it("when a & b both are numbers for input ", () => {
+    const result = sum(5,5);
+    expect(result).toBe(10);
+});
+
+it("when a & b both are Strings for input ", () => {
+    const result = sum("5","5");
+    expect(result).toBe(10);
+});
+
+// Assertion Test Case in React
+describe('Addition', () => {
+    it('We know that 2 + 2 makes 4 ', () => {
+        expect( 2 + 2 ).toBe(4);
+    });
+});
+
+describe('Subtraction', () => {
+    it('We know that 10 - 5 makes 5 ', () => {
+        expect( 10 - 5 ).toBe(5);
+    });
+});
+
+describe('Multiplication', () => {
+    it('We know that 5 * 2 makes 10 ', () => {
+        expect( 5 * 2 ).toBe(10);
+    });
+});
+
+describe('Division', () => {
+    it('We know that 10 / 2 makes 5 ', () => {
+        expect( 10 / 2 ).toBe(5);
+    });
+});
+
+describe('Modulus', () => {
+    it('We know that 10 % 5 makes 0 ', () => {
+        expect( 10 % 5 ).toBe(0);
+    });
+});
